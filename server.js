5dd4055e2db2cc89a7f0dc7df617167ffc1d9af8
unknown <matthewjs@outlook.com>
2019-07-17 23:28:32 +0100

    clean up code but still same functionality

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Dependencies
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -8,20 +7,21 @@ var server = http.Server(app);
 var io = socketIO(server);
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
+
 // Routing
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
+
 // Starts the server.
 server.listen(5000, function() {
   console.log('Starting server on port 5000');
 });
-// Add the WebSocket handlers
-io.on('connection', function(socket) {
-});
+
+// io emit => send to all
 setInterval(function() {
-  io.sockets.emit('message', 'hi!');
-}, 1000);
+  io.sockets.emit('state', players);
+}, 1000 / 60);
 
 var colours = ['red','blue','green','yellow','pink'];
 
@@ -32,37 +32,33 @@ function randomFromArray(items)
 
 //Player info
 var players = {}
-io.on('connection', function(socket) {
-  socket.on('new player', function() {
-	  var coloura = randomFromArray(colours);
-    players[socket.id] = {
-      x: 300,
-      y: 300,
-	  colour: coloura,
-    };
-  });
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-    if (data.left) {
-      player.x -= 5;
-    }
-    if (data.up) {
-      player.y -= 5;
-    }
-    if (data.right) {
-      player.x += 5;
-    }
-    if (data.down) {
-      player.y += 5;
-    }
-  });
-});
-setInterval(function() {
-  io.sockets.emit('state', players);
-}, 1000 / 60);
-
-io.on('connection', function(socket) {
-  socket.on('disconnect', function() {
-	delete players[socket.id];   
+//io.on -> when read by everyone (so everyone triggers this)
+io.on('connection', function(socket) {	
+//socket.on -> specific socket reads (e.g. new player connects, this only runs once for that player.)
+	socket.on('new player', function() {
+		var coloura = randomFromArray(colours);
+		players[socket.id] = {
+			x: 300,
+			y: 300,
+			colour: coloura,
+			};
+		})
+	socket.on('disconnect', function() {
+		delete players[socket.id];   
+		});
+	socket.on('movement', function(data) {
+		var player = players[socket.id] || {};
+		if (data.left) {
+		player.x -= 5;
+		}
+		if (data.up) {
+		player.y -= 5;
+		}
+		if (data.right) {
+		player.x += 5;
+		}
+		if (data.down) {
+		player.y += 5;
+		}
   });
 });
